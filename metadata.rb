1fa7c88f12dc896c692dd6924da2adbd36f1e913
core-cookbooks <nagalakshmi.n@cloudenablers.com>
2015-04-20 18:39:15 +0530

    Initial Release

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer 'Cloudenablers'
+maintainer_email 'nagalakshmi.n@cloudenablers.com'
+license 'All rights reserved'
+description 'Provides an LWRP for managing the /etc/hosts file'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.1.1'
